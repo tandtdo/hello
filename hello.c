09028a849115e24d95c11838abc90113f477d68c
tandtdo <tandtdo@gmail.com>
2026-08-25 09:40:31 -0500

    fixed standard library bug

--- hello.c
+++ hello.c
@@ -1,11 +1,14 @@
 /**
  * Basic Hello World program
  */
+#include <stdlib.h>
+#include <stdio.h>
+
 
  int main(int argc, char **argv) {
 
     printf("Hello, I'm Tan Do!\n");
-    printf("Major is Computer Engineering! \n");
+    printf("Major is Computer Engineering!\n");
 
     return 0;
  }
