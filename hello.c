68f18839329e0377aac528399a57500067dba514
tandtdo <tandtdo@gmail.com>
2026-08-25 09:25:00 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**
+ * Basic Hello World program
+ */
+
+ int main(int argc, char **argv) {
+
+    printf("Hello, I'm Tan Do!\n");
+    printf("Major is Computer Engineering! \n");
+
+    return 0;
+ }
